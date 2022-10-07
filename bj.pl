3679363110f56b0687bd021e4d7dbb4f964164af
PedroB <bereilhp@gmail.com>
2022-10-07 14:00:52 +0200

    Cartas y estructura fichero bj

--- bj.pl
+++ bj.pl
@@ -1,8 +1,24 @@
-
-
-
 % Rules
+% ; = OR
+% , = AND
 
-
+carta(X,Y) :-
+    cartas(X,Y).
 
 % Facts
+
+cartas(1,1). % Carta y su valor
+cartas(2,2).
+cartas(3,3).
+cartas(4,4).
+cartas(5,5).
+cartas(6,6).
+cartas(7,7).
+cartas(8,8).
+cartas(9,9).
+cartas(10,10).
+cartas(j,10).
+cartas(q,10).
+cartas(k,10).
+cartas(a,1).
+cartas(a,11).
